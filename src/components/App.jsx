4cda1c2de4d29946c19ac048bb93c1cf04086298
Anastasia Les <anastasia@sonarmedia.com>
2022-04-05 09:14:55 +0200

    tasks

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -11,7 +11,7 @@ export const App = () => {
         color: '#010101',
       }}
     >
-      React homework 01
+      React homework template
     </div>
   );
 };
--- src/components/App/App.styled.js
+++
@@ -1,9 +0,0 @@
-import styled from '@emotion/styled'
-
-export const Container = styled.div`
-/* background-size: contain; */
-background-color: #E8E8E8;
-padding-top: 30px;
-display: flex;
-height: 100%;
-`;
