009bb50ebc87afacb1e8823af3563b7ab7f23118
Anastasia Les <anastasia@sonarmedia.com>
2022-04-05 11:35:44 +0200

    profile fix

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,8 +1,6 @@
 import { Container } from './App.styled';
 import { SocialProfile } from '../../components/SocialProfile/SocialProfile';
-// import { Statistics } from 'components/Statistics/Statistics';
 import user from '../../social-profile/user.json';
-// import stats from '../../statistics/data.json'
 
 export const App = () => {
   return (
@@ -16,9 +14,6 @@ export const App = () => {
         views={user.stats.views}
         likes={user.stats.likes}
       />
-      {/* <Statistics
-        key={stats.id}
-        stats={stats} /> */}
     </Container>
   );
 };
---
+++ src/components/App/App.styled.js
@@ -0,0 +1,9 @@
+import styled from '@emotion/styled';
+
+export const Container = styled.div`
+  /* background-size: contain; */
+  background-color: #e8e8e8;
+  padding-top: 30px;
+  display: flex;
+  height: 100%;
+`;
